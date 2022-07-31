a2e1c8fad183541d729f7c2c97d9868fc856563d
mhellnerdev <mhellnerdev@gmail.com>
2022-07-31 14:29:44 +0000

    basic code to connect bot

---
+++ bot-connect.py
@@ -0,0 +1,23 @@
+# bot.py
+import os
+import asyncio
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  guild = discord.utils.get(client.guilds, name=GUILD)
+  print(
+    f'{client.user} is connected to the following guild:\n'
+    f'{guild.name}(id: {guild.id})'
+  )
+
+
+client.run(TOKEN)
